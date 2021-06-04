ae5ced78f52e6bb209bf8d2893b4a0339a55e2ef
Sebastian Rizzuto <srizzuto@gmail.com>
2021-06-04 06:05:21 -0300

    Add editing and errors display

--- app/helpers/users_helper.rb
+++ app/helpers/users_helper.rb
@@ -1,2 +1,21 @@
 module UsersHelper
+
+  def emailError
+    @user.errors.full_messages_for(:email).each do |msg|
+      msg
+    end
+  end
+
+  def usernameError
+    @user.errors.full_messages_for(:username).each do |msg|
+      msg
+    end
+  end
+
+  def passwordError
+    @user.errors.full_messages_for(:password).each do |msg|
+      msg
+    end
+  end
+
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -1,5 +1,5 @@
 class User < ApplicationRecord
-  validates :username, presence: true, length: { minimum: 6, message: "Insert at least 6 characters" }
+  validates :username, presence: true
   validates :email, presence: true, format: { with: URI::MailTo::EMAIL_REGEXP }
-  validates :password, presence: true, length: { minimum: 6, message: "Insert at least 6 characters" }
+  validates :password, presence: true
 end
